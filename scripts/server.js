dc2c1d64e0db48c4bf306ce4de33ce033a54ad87
shirin faile <shirin.faile@hotmail.com>
2024-03-11 11:41:33 +0100

    added queries

--- scripts/server.js
+++ scripts/server.js
@@ -5,6 +5,7 @@ const fs = require(`fs`)
 http.createServer((req,res) => {
 
     const parsedUrl = url.parse(req.url, true);
+    const queryParameters = parsedUrl.query;
 
     let contentType = 'text/html';
     if (parsedUrl.pathname.endsWith('.css')) {
@@ -16,9 +17,28 @@ http.createServer((req,res) => {
     let filePath = `.${parsedUrl.pathname}`;
     if (filePath === './') {
         filePath = './index.html';
-    } else {
-        console.log('Error: Invalid file path or request');
     }
+        
+    if (queryParameters.name) {
+        const name = queryParameters.name
+
+        if (name === 'corgi') {
+            filePath = './html/corgi.html';
+
+        } else if (name === 'pride') {
+            filePath = './html/pride.html';
+
+        } else if (name === 'space') {
+            filePath = './html/space.html';
+
+        } else if (name === 'watermelon') {
+            filePath = './html/watermelon.html'
+
+        } else {
+            console.log('Error: Invalid file path or request')
+        }
+   
+    }    
     
     fs.readFile(filePath, (err, data) => {
     
